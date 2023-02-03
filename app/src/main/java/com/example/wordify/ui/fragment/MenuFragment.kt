a30f33f546fea620f287cbcba92d69662bf199b3
xen <selegakamil@gmail.com>
2023-02-03 23:32:48 +0100

    Add navigate actions to buttons, add stats fragment

--- app/src/main/java/com/example/wordify/ui/fragment/MenuFragment.kt
+++ app/src/main/java/com/example/wordify/ui/fragment/MenuFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.wordify.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -17,7 +18,15 @@ class MenuFragment : Fragment() {
     ): View {
         binding = FragmentMenuBinding.inflate(inflater, container, false)
 
+        binding.playButton.setOnClickListener {
+            val action = MenuFragmentDirections.actionMenuFragmentToGameFragment()
+            it.findNavController().navigate(action)
+        }
 
+        binding.statsButton.setOnClickListener {
+            val action = MenuFragmentDirections.actionMenuFragmentToStatsFragment()
+            it.findNavController().navigate(action)
+        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/wordify/ui/fragment/StatsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.wordify.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wordify.databinding.FragmentStatsBinding
+
+class StatsFragment : Fragment() {
+    private lateinit var binding: FragmentStatsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentStatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
